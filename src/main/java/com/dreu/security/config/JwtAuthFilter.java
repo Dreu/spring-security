7fba3f2793b41655c7b2ece509f63ac91437299f
abdoul <khadrygassama@gmail.com>
2023-01-04 22:48:06 -0500

    filter: ajout du filter pour recuperer et traiter le token jwt

---
+++ src/main/java/com/dreu/security/config/JwtAuthFilter.java
@@ -0,0 +1,40 @@
+package com.dreu.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
+import org.springframework.security.web.header.Header;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.reactive.function.server.ServerRequest;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private static final String BEARER_CONST = "Bearer ";
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String jwt;
+        final String userEmail;
+
+        if(authHeader == null || !authHeader.startsWith(BEARER_CONST)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(BEARER_CONST.length());
+        userEmail = ""; // TODO add class than can manipulate a JWT token
+    }
+}
